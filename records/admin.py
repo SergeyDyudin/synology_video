bf28f71881f1ed2f376e4c46c073347f8a9ac051
Sergey Dyudin <sergeydyudin@yandex.ru>
2021-11-17 11:03:02 +0300

    Adding CustomerUserAdmin
    Переопредлен класс UserAdmin для того, чтобы в админке пользователи не
    могли сами себя добавлять в superuser.

--- records/admin.py
+++ records/admin.py
@@ -1,6 +1,30 @@
-from django.contrib import admin
-
 from .models import *
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+# Register out own model admin, based on the default UserAdmin
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()  # type: Set[str]
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+            }
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+        return form
 
 
 class RecordsAdmin(admin.ModelAdmin):
